54188633131716cc8084dae55c6cca8b678cc67d
Ngo Quang Ha <ngoquangha0811xxz@gmail.com>
2024-11-07 22:19:42 +0700

    Tìm kiếm phim

--- routers/client/index.router.js
+++ routers/client/index.router.js
@@ -2,6 +2,7 @@
 const homeRouter = require("./home.router");
 const movieRouter = require("./movie.router");
 const userRouter = require("./user.router");
+const searchRouter = require("./search.router");
 const userMiddleware = require("../../middlewares/client/user.middleware");
 
 module.exports = (app) => {
@@ -12,4 +13,6 @@ module.exports = (app) => {
     app.use("/movies", movieRouter);
 
     app.use("/user", userRouter);
+
+    app.use("/search", searchRouter);
 };
